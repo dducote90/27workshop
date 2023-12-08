a76907b41f2d50d1528b9ab1791b265d630bd120
dducote90 <git@github.com:dducote90/resume.git>
2023-12-07 18:48:57 -0600

    final commit

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -7,6 +7,21 @@ const SignUpForm = ({ setToken }) => {
 
     async function handleSubmit(event) {
         event.preventDefault();
+
+        // Simple form validation
+        if (username.trim() === "") {
+            setError("Username cannot be empty.");
+            return;
+            
+        
+        }
+
+        if (password.length < 6) {
+            setError("Password must be at least 6 characters long.");
+            return;
+        }
+
+        // Make API call to sign up
         try {
             const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
                 method: "POST",
@@ -20,27 +35,37 @@ const SignUpForm = ({ setToken }) => {
             });
 
             const result = await response.json();
-            setToken(result.token)
-            console.log(result);
+
+            if (result.success) {
+                // If signup is successful, set the token
+                setToken(result.token);
+                console.log(result.message);
+            } else {
+                setError(result.message);
+            }
         } catch (error) {
-            setError(error.message);
+            setError("Error occurred while signing up.");
         }
     }
 
     return (
         <div>
             <h2>Sign Up!</h2>
-            {error && <p>{error}</p>}
+            {error && <p style={{ color: "red" }}>{error}</p>}
             <form onSubmit={handleSubmit}>
                 <label>
-                    Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
+                    Username:{" "}
+                    <input value={username} onChange={(e) => setUsername(e.target.value)} />
                 </label>
                 <br />
                 <label>
-                    Password: <input value={password} onChange={(e) => setPassword(e.target.value)} />
+                    Password:{" "}
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </label>
                 <br />
-                <button type="submit">Submit</button>
+                <button type="submit" disabled={username.length < 7 || !/\d/.test(password) || !/[a-zA-Z]/.test(password)}>
+                    Submit
+                </button>
             </form>
         </div>
     );
